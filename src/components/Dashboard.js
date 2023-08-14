e81fec49d5dd31cfc59826dbb9072d620be822dd
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-08-14 12:16:07 -0700

    Add large panel click functionality to return to 4 panel view

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -35,9 +35,9 @@ class Dashboard extends Component {
   };
 
   selectPanel(id) {
-    this.setState({
-      focused: id
-    });
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null: id
+    }));
   }
   
   render() {
@@ -56,7 +56,7 @@ class Dashboard extends Component {
           id={panel.id}
           label={panel.label}
           value={panel.value}
-          onSelect={this.selectPanel}
+          onSelect={event => this.selectPanel(panel.id)}
         />
       ));
 
